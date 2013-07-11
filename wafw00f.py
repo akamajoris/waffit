ede02a389186f17107b895373e36e4330f8f4020
sandrogauc@gmail.com <sandrogauc@gmail.com@342d8fd8-3b52-11de-b438-597f59cd7555>
2013-07-11 11:53:20 +0000

    fixed secureIIS detection

--- wafw00f.py
+++ wafw00f.py
@@ -291,12 +291,18 @@ class WafW00F(waftoolsengine):
     def issecureiis(self):
         # credit goes to W3AF
         detected = False
+        r = self.normalrequest()
+        if r is None:
+            return
+        response,responsebody = r
+        if response.status == 404:
+            return
         headers = dict()
         headers['Transfer-Encoding'] = 'z' * 1025
         r = self.normalrequest(headers=headers)
         if r is None:
             return 
-        response,responsebody = r 
+        response,responsebody = r         
         if response.status == 404:
             detected = True
         return detected
